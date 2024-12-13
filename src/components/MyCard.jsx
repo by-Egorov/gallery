e6e65e8fa2b6fd18aee87a15fe017f22a2997a1c
Александр <109337223+by-Egorov@users.noreply.github.com>
2024-12-13 14:22:37 +0300

    правка стилей карточки

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,77 +1,80 @@
 import React from 'react'
 import { DeleteFilled, StarFilled, LikeFilled } from '@ant-design/icons'
 import { Card } from 'antd'
+
 const { Meta } = Card
 const MyCard = ({
-	image,
-	title,
-	description,
-	id,
-	like,
-	favorite,
-	setLike,
-	setFavorite,
-	removedCard,
-	cardById,
+  image,
+  title,
+  description,
+  id,
+  like,
+  favorite,
+  setLike,
+  setFavorite,
+  removedCard,
+  cardById,
 }) => (
-	<Card
-		style={{
-			padding: 15,
-			width: 300,
-			height: 405,
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'space-between',
-		}}
-		cover={
-			<img
-				alt='example'
-				src={image}
-				style={{ width: '100%', height: '200px', objectFit: 'contain' }}
-			/>
-		}
-		onClick={() => cardById(id)}
-		actions={[
-			<LikeFilled
-				key='like'
-				onClick={e => {
-					e.stopPropagation()
-					setLike(id)
-				}}
-				style={{ color: like ? '	#3F00FF' : '' }}
-			/>,
-			<StarFilled
-				key='favorite'
-				onClick={e => {
-					e.stopPropagation()
-					setFavorite(id)
-				}}
-				style={{ color: favorite ? '#ffd533' : '' }}
-			/>,
-			<DeleteFilled
-				key='delete'
-				onClick={e => {
-					e.stopPropagation()
-					removedCard(id)
-				}}
-				style={{ color: '#E34234' }}
-			/>,
-		]}>
-		<Meta
-			title={title}
-			description={
-				<div
-					style={{
-						overflow: 'hidden',
-						textOverflow: 'ellipsis',
-						display: '-webkit-box',
-						WebkitLineClamp: 3,
-						WebkitBoxOrient: 'vertical',
-					}}>
-					{description}
-				</div>
-			}
-		/>
-	</Card>
+  <Card
+    style={{
+      padding: 15,
+      width: 300,
+      height: 405,
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}
+    cover={
+      <img
+        alt='example'
+        src={image}
+        style={{ width: '100%', height: '200px', objectFit: 'contain' }}
+      />
+    }
+    onClick={() => cardById(id)}
+    actions={[
+      <LikeFilled
+        key='like'
+        onClick={e => {
+          e.stopPropagation()
+          setLike(id)
+        }}
+        style={{ color: like ? '	#3F00FF' : '' }}
+      />,
+      <StarFilled
+        key='favorite'
+        onClick={e => {
+          e.stopPropagation()
+          setFavorite(id)
+        }}
+        style={{ color: favorite ? '#ffd533' : '' }}
+      />,
+      <DeleteFilled
+        key='delete'
+        onClick={e => {
+          e.stopPropagation()
+          removedCard(id)
+        }}
+        style={{ color: '#E34234' }}
+      />,
+    ]}
+  >
+    <Meta
+      title={title}
+      description={
+        <div
+          style={{
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: 3,
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
+          {description}
+        </div>
+      }
+    />
+  </Card>
 )
 export default MyCard
--- src/page/CardItem.jsx
+++ src/page/CardItem.jsx
@@ -1,107 +1,110 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { Card, Button } from 'antd'
 import { DeleteFilled, StarFilled, LikeFilled } from '@ant-design/icons'
-import {setLike, setFavorite, removeCard} from '../store/cardActions'
+import { setLike, setFavorite, removeCard } from '../store/cardActions'
 
 const { Meta } = Card
 
 const CardItem = () => {
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const { id } = useParams()
-	const cards = useSelector(state => state.cards)
-	const [card, setCard] = useState(null)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const cards = useSelector(state => state.cards)
+  const [card, setCard] = useState(null)
 
-	const setLikeHandler = (id) => {
-		dispatch(setLike(id)) 
-	}
+  const setLikeHandler = id => {
+    dispatch(setLike(id))
+  }
 
-	const setFavoriteHandler = (id) => {
-		dispatch(setFavorite(id)) 
-	}
-	const setRemoveCardHandler = (id) => {
-		dispatch(removeCard(id)) 
-	}
+  const setFavoriteHandler = id => {
+    dispatch(setFavorite(id))
+  }
+  const setRemoveCardHandler = id => {
+    dispatch(removeCard(id))
+  }
 
+  useEffect(() => {
+    if (Array.isArray(cards)) {
+      const foundCard = cards.find(card => card.id === Number(id))
+      setCard(foundCard)
+    }
+  }, [id, cards])
 
-	useEffect(() => {
-		if (Array.isArray(cards)) {
-			const foundCard = cards.find(card => card.id === Number(id))
-			setCard(foundCard)
-		}
-	}, [id, cards])
+  useEffect(() => {
+    const scrollPosition = location.state?.scrollPosition || 0
+    window.scrollTo(0, scrollPosition)
+  }, [location.state])
 
-
-	return (
-		<>
-			{card ? (
-				<div
-					style={{
-						padding: 25,
-						display: 'flex',
-						justifyContent: 'center',
-					}}>
-					<Card
-						cover={
-							<img
-								alt='example'
-								src={card.image}
-								style={{ width: '100%', height: '360px', objectFit: 'contain' }}
-							/>
-						}
-						style={{
-							padding: 15,
-							width: '800px',
-							height: '100%',
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'space-between',
-						}}
-						actions={[
-							<LikeFilled
-								key='like'
-								onClick={e => {
-									e.stopPropagation()
-									setLikeHandler(card.id)
-								}}
-								style={{ color: card.like ? '#3F00FF' : '' }}
-							/>,
-							<StarFilled
-								key='favorite'
-								onClick={e => {
-									e.stopPropagation()
-									setFavoriteHandler(card.id)
-								}}
-								style={{ color: card.favorite ? '#ffd533' : '' }}
-							/>,
-							<DeleteFilled
-								key='delete'
-								onClick={e => {
-									e.stopPropagation()
-									setRemoveCardHandler(card.id)
-								}}
-								style={{ color: '#E34234' }}
-							/>,
-							<Button onClick={() => navigate(-1)}>Назад</Button>,
-						]}>
-						<Meta
-							title={card.title}
-							description={
-								<div style={{ minHeight: '50px' }}>{card.description}</div>
-							}
-						/>
-					</Card>
-				</div>
-			) : (
-				<div style={{ padding: '20px' }}>
-					<p>Карточка не найдена</p>
-					<Link to='/'>Назад</Link>
-				</div>
-			)}
-		</>
-	)
+  return (
+    <>
+      {card ? (
+        <div className='card__wrapper'>
+          <Card
+            className='card-item'
+            cover={
+              <img
+                alt='example'
+                src={card.image}
+                style={{ width: '100%', height: '360px', objectFit: 'contain' }}
+              />
+            }
+            style={{
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'space-between',
+            }}
+            actions={[
+              <LikeFilled
+                key='like'
+                onClick={e => {
+                  e.stopPropagation()
+                  setLikeHandler(card.id)
+                }}
+                style={{ color: card.like ? '#3F00FF' : '' }}
+              />,
+              <StarFilled
+                key='favorite'
+                onClick={e => {
+                  e.stopPropagation()
+                  setFavoriteHandler(card.id)
+                }}
+                style={{ color: card.favorite ? '#ffd533' : '' }}
+              />,
+              <DeleteFilled
+                key='delete'
+                onClick={e => {
+                  e.stopPropagation()
+                  setRemoveCardHandler(card.id)
+                }}
+                style={{ color: '#E34234' }}
+              />,
+              <Button onClick={() => navigate(-1)}>Назад</Button>,
+            ]}
+          >
+            <Meta
+              title={
+                <div style={{ textAlign: 'center', whiteSpace: 'wrap' }}>
+                  {card.title}
+                </div>
+              }
+              description={
+                <div style={{ minHeight: '50px' }}>{card.description}</div>
+              }
+            />
+          </Card>
+        </div>
+      ) : (
+        <div style={{ padding: '20px' }}>
+          <p>Карточка не найдена</p>
+          <Link onClick={() => navigate(-1)}>Назад</Link>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default CardItem
